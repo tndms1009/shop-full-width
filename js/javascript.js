eebd2a7eb82b172275264f2ba2d73bfd3cafc3fc
tndms1009 <tndms1009@naver.com>
2016-02-03 14:19:54 +0900

    2

---
+++ js/javascript.js
@@ -0,0 +1,54 @@
+$(function(){
+    $("#search").attr('placeholder','Enter Search...');
+        
+        var placeholder = $("input[placeholder]");
+
+            function makePlaceHolder(){
+                placeholder.each(function(i) {
+                    var parHeight = placeholder.eq(i).css("height");
+                    var parLineHeight = placeholder.eq(i).css("lineHeight");
+                    var parPadd = placeholder.eq(i).css("padding");
+                    var parWidth = placeholder.eq(i).css("width");
+                    var parTxtIn = placeholder.eq(i).css("textIndent");
+                    var parColor = placeholder.eq(i).css("color");
+                    var parFontSize = placeholder.eq(i).css("fontSize");
+                    placeholder.eq(i).wrap("<div class='placehold' style='position:relative;color:#555;'></div>");
+                    $(".placehold").eq(i).append("<span>"+placeholder.eq(i).attr("placeholder")+"</span>");
+                    placeholder.eq(i).next().css({
+                            "height":parHeight,
+                            "lineHeight":parLineHeight,
+                            "padding":parPadd,
+                            "width":parWidth,
+                            "textIndent":parTxtIn,
+                            "color":parColor,
+                            "fontSize":parFontSize,
+                            "position":"absolute",
+                            "left":1,
+                            "top":1,
+                            "display":"block"
+                        });
+                });
+            };
+            makePlaceHolder();
+
+            placeholder.each(function(i) {
+                var holderVal=placeholder.eq(i).val().length;
+                if(holderVal==0){
+                    $(this).next("span").show();
+                }
+                placeholder.bind("focusin",function(){
+                    $(this).next("span").hide();
+                    placeholder.not(this).filter(function(index){return $(this).val().length==0}).next("span").show();
+                });
+            });
+
+            $(".placehold span").click(function(){
+                $(this).prev().focus();
+            });
+
+        
+
+
+    
+    
+});
